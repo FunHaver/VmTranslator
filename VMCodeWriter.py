dad5aa0ef0276ccbb47f3a95932ab76a912f9fab
Conor Sullivan <conor@conorsullivan.net>
2024-06-11 10:55:43 -0500

    Interfaces

--- VMCodeWriter.py
+++ VMCodeWriter.py
@@ -1,3 +1,15 @@
 class VMCodeWriter:
     def __init__(self):
         print("Initialized CodeWriter")
+
+    # Writes to the output file the 
+    # assembly code that implements the given
+    # arithmetic or logic command
+    def writeArithmetic(self, command):
+        return None
+    
+    # Writes to the output file the 
+    # assembly code that implements the given
+    # push or pop command
+    def writePushPop(self, command, segment, index):
+        return None
--- VMParser.py
+++ VMParser.py
@@ -2,3 +2,27 @@ class VMParser:
 
     def __init__(self):
         print("Initialized Parser")
+
+    # Any more lines in the input
+    def hasMoreLines(self):
+        return False;
+
+    # Reads next command from input and makes it the
+    # current command
+    def advance(self):
+        return None
+    
+    # Returns the type of the current command (C_FOO)
+    def commandType(self):
+        return None
+    
+    # Returns the first part of the current command
+    # Should not be called if the command type is C_RETURN
+    def arg1(self):
+        return None
+
+    # Returns second part of the command, should only be called if
+    # command is of type C_PUSH, C_POP, C_FUNCTION, C_CALL
+    def arg2(self):
+        return None
+    
