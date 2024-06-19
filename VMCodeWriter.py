d02000ac1c22c7de193ae249b4b8df31600f1af3
Conor Sullivan <conor@conorsullivan.net>
2024-06-19 15:11:32 -0500

    Rough together something that can add

--- VMCodeWriter.py
+++ VMCodeWriter.py
@@ -11,6 +11,20 @@ class VMCodeWriter:
         self.outFile.write(asmString)
         self.outFile.write(os.linesep)
 
+    def __push(self, segment, index):
+        # D=index
+        if segment == "constant":
+            self.__asmOut("@" + str(index))
+            self.__asmOut("D=A")
+        else:
+            return
+        # *SP=D
+        self.__asmOut("@SP")
+        self.__asmOut("A=M")
+        self.__asmOut("M=D")
+        # SP++
+        self.__asmOut("@SP")
+        self.__asmOut("M=M+1")
     # Writes to the output file the 
     # assembly code that implements the given
     # arithmetic or logic command
@@ -18,6 +32,30 @@ class VMCodeWriter:
         vmComment = "// " + command
         self.__asmOut(vmComment)
 
+        if command == "add":
+            # SP--
+            self.__asmOut("@SP")
+            self.__asmOut("M=M-1")
+            # RAM[i] = *SP
+            self.__asmOut("A=M")
+            self.__asmOut("D=M")
+            self.__asmOut("@operand_2")
+            self.__asmOut("M=D")
+            # SP--
+            self.__asmOut("@SP")
+            self.__asmOut("M=M-1")
+            # add two stack items
+            self.__asmOut("A=M")
+            self.__asmOut("D=M")
+            self.__asmOut("@operand_2")
+            self.__asmOut("D=D+M")
+            # *SP = D
+            self.__asmOut("@SP")
+            self.__asmOut("A=M")
+            self.__asmOut("M=D")
+            # SP++
+            self.__asmOut("@SP")
+            self.__asmOut("M=M+1")
     
     # Writes to the output file the 
     # assembly code that implements the given
@@ -25,4 +63,7 @@ class VMCodeWriter:
     def writePushPop(self, command, segment, index):
         vmComment = "// " + command + " " + segment + " " + str(index)
         self.__asmOut(vmComment)
+        
+        if command == "push":
+            self.__push(segment, index)
 
